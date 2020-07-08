27b352ea53f07d59d7cf0fe42146cb1dec4c697a
psingh1028 <pawanjotsingh1997@gmail.com>
2020-07-07 20:09:23 -0400

    added GetPlayerName

--- Number Guess/Number Guess/Program.cs
+++ Number Guess/Number Guess/Program.cs
@@ -10,6 +10,30 @@ namespace Number_Guess
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to number guess");
+            string player1Name = GetPlayerName(1);
+            string player2Name = GetPlayerName(2);
+
+
+            Console.ReadKey();
+
+
+        }
+
+        public static string GetPlayerName(int playerNumber)///creates method that takes in a player number weather it is 1 or 2 
+        {
+            string playerName="";
+            Console.WriteLine("Player {0} what is your name", playerNumber);///asks users to enter their name 
+            try///Reads the input by the player
+            {
+               playerName = Console.ReadLine();
+              
+            }
+            catch( Exception e)
+            {
+                Console.WriteLine("Looks like something went wrong");
+            }
+            return playerName;//returns player name to method call
         }
     }
 }
